faba4a3a9ced425366eb4adb88b734dff4ed26ba
rovshan <rouvsen@gmail.com>
2023-04-30 17:53:05 +0400

    initial commit

---
+++ src/main/java/com/rouvsen/JpaRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.rouvsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rouvsen/JpaRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rouvsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
